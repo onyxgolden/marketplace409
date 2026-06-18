7cadf294e49a17485c9c5c69f273b67e31bd71c9
Jason Morgan <jasonmorgan99@gmail.com>
2026-06-17 23:20:31 -0500

    Add domain structure blueprint

---
+++ src/constants/domainStructure.js
@@ -0,0 +1,17 @@
+export const DOMAIN_FILES = [
+  "Entity",
+  "Types",
+  "Validator",
+  "Repository",
+  "Service",
+  "Events",
+  "index",
+];
+
+export const OPTIONAL_DOMAIN_FILES = [
+  "Policies",
+  "Queries",
+  "Commands",
+  "Factory",
+  "Mapper",
+];
