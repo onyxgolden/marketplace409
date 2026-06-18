1b73f463af5eaf9f9b7fd13db90421c754ef5c9e
Jason Morgan <jasonmorgan99@gmail.com>
2026-06-18 17:45:45 -0500

    Add canonical account types

---
+++ src/domains/ledger/constants/AccountTypes.js
@@ -0,0 +1,14 @@
+/**
+ * AccountTypes
+ *
+ * Canonical account classifications used throughout
+ * Financial Forge.
+ */
+
+export const AccountTypes = Object.freeze({
+  ASSET: "ASSET",
+  LIABILITY: "LIABILITY",
+  EQUITY: "EQUITY",
+  REVENUE: "REVENUE",
+  EXPENSE: "EXPENSE",
+});
---
+++ src/domains/ledger/constants/index.js
@@ -0,0 +1,1 @@
+export { AccountTypes } from "./AccountTypes";
