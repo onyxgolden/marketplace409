184af2a718459d19a9489e33d39807c35c8f94fe
Jason Morgan <jasonmorgan99@gmail.com>
2026-06-21 16:12:03 -0500

    Make balance sheet a financial report

--- src/domains/ledger/reports/BalanceSheet.js
+++ src/domains/ledger/reports/BalanceSheet.js
@@ -1,19 +1,35 @@
 import { AccountBalanceCollection } from "./AccountBalanceCollection";
+import { FinancialReport } from "./FinancialReport";
+import { ReportLine } from "./ReportLine";
 
 /**
  * BalanceSheet
  *
  * Immutable representation of a balance sheet.
  * Values originate exclusively from calculated ledger balances.
+ *
+ * A BalanceSheet is a financial report wrapper
+ * around a collection of AccountBalance objects.
  */
 
-export class BalanceSheet {
+export class BalanceSheet extends FinancialReport {
   constructor(accountBalances) {
     if (!(accountBalances instanceof AccountBalanceCollection)) {
       throw new Error(
         "BalanceSheet requires an AccountBalanceCollection"
       );
     }
+
+    super({
+      name: "Balance Sheet",
+      lines: accountBalances.all().map(
+        (accountBalance) =>
+          new ReportLine({
+            label: accountBalance.accountId,
+            amount: accountBalance.balance,
+          })
+      ),
+    });
 
     this.accountBalances = accountBalances;
 
