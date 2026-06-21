fcbf2cf7b1034668a6cb805c9d4307829dafb62b
Jason Morgan <jasonmorgan99@gmail.com>
2026-06-21 16:16:05 -0500

    Make income statement a financial report

--- src/domains/ledger/reports/IncomeStatement.js
+++ src/domains/ledger/reports/IncomeStatement.js
@@ -1,19 +1,35 @@
 import { AccountBalanceCollection } from "./AccountBalanceCollection";
+import { FinancialReport } from "./FinancialReport";
+import { ReportLine } from "./ReportLine";
 
 /**
  * IncomeStatement
  *
  * Immutable representation of an income statement.
  * Values originate exclusively from calculated ledger balances.
+ *
+ * An IncomeStatement is a financial report wrapper
+ * around a collection of AccountBalance objects.
  */
 
-export class IncomeStatement {
+export class IncomeStatement extends FinancialReport {
   constructor(accountBalances) {
     if (!(accountBalances instanceof AccountBalanceCollection)) {
       throw new Error(
         "IncomeStatement requires an AccountBalanceCollection"
       );
     }
+
+    super({
+      name: "Income Statement",
+      lines: accountBalances.all().map(
+        (accountBalance) =>
+          new ReportLine({
+            label: accountBalance.accountId,
+            amount: accountBalance.balance,
+          })
+      ),
+    });
 
     this.accountBalances = accountBalances;
 
