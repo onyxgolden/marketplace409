b254b7e897c6cc53c73cb84391be2f2f1e4cdc2f
Jason Morgan <jasonmorgan99@gmail.com>
2026-07-09 17:00:12 -0500

    Centralize financial application exports

--- src/application/financial/index.js
+++ src/application/financial/index.js
@@ -5,5 +5,8 @@ export { FinancialDashboardIntelligenceApplication } from "./FinancialDashboardI
 export { FinancialReadModelApplication } from "./read-models/FinancialReadModelApplication.js";
 export { FinancialIntelligenceApplication } from "./FinancialIntelligenceApplication.js";
 export { FinancialOperationsApplication } from "./FinancialOperationsApplication.js";
+export { FinancialImportApplication } from "./FinancialImportApplication.js";
+export { TransactionReviewApplication } from "./TransactionReviewApplication.js";
+export { FinancialSnapshotViewApplication } from "./FinancialSnapshotViewApplication.js";
 export { ForgeDashboardApplication } from "./ForgeDashboardApplication.js";
 export { ForgeFinancialDashboardApplication } from "./ForgeFinancialDashboardApplication.js";
